a5a35ed99d7744560bde7f570aefa45c5e64116a
refutaremos <institucional.luisantonio@gmail.com>
2026-09-03 12:55:47 -0300

    Rename and simplify volume calculation function

--- programacao/setembro/03-11-2026/questao1.cpp
+++ programacao/setembro/03-11-2026/questao1.cpp
@@ -1,15 +1,5 @@
 #include <iostream>
-#include <cmath>
 
-double calculaVolumeEsfera(double raio) {
-    const double PI = 3.141592653589793;
-    return (4.0 / 3.0) * PI * pow(raio, 3);
-}
-
-int main() {
-    double raio;
-    std::cout << "Digite o raio da esfera: ";
-    std::cin >> raio;
-    std::cout << "Volume: " << calculaVolumeEsfera(raio) << "\n";
-    return 0;
+double volumeEsfera(double raio) {
+    return (4.0 / 3.0) * 3.14159 * (raio * raio * raio);
 }
